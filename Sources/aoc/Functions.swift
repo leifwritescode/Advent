65cc901fe81c956666502e5c27a378bac6dddb0f
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2022-06-14 20:20:26 +0100

    Implement arbitrary array transposition functions

--- Sources/aoc/Functions.swift
+++ Sources/aoc/Functions.swift
@@ -28,3 +28,42 @@ func timed(toLog log: Log, _ closure: () -> Void) -> TimeInterval {
     log.info(theMessage: "Done. Execution took \(deltaAsString).")
     return delta
 }
+
+func transpose1d<Type>(array: [Type], width: Int, height: Int) -> [Type?] {
+    guard width * height == array.count else {
+        return []
+    }
+
+    var transposed = [Type?](repeating: nil, count: width * height)
+    for c in stride(from: 0, through: width - 1, by: 1) {
+        for r in stride(from: 0, through: height - 1, by: 1) {
+            transposed[c * height + r] = array[r * width + c]
+        }
+    }
+
+    return transposed
+}
+
+/**
+  transposes an 2d array of w x h to a 2d array of h x w
+ */
+func transpose2d<Type>(array: [[Type]], width: Int, height: Int) -> [[Type?]] {
+    guard array.count > 0 else {
+        return [[]]
+    }
+
+    let arraySize = array.count * array[0].count
+    guard width * height == arraySize else {
+        return [[]]
+    }
+
+    // transpose the array from w x h to h x w
+    var transposed = [[Type?]](repeating: Array(repeating: nil, count: height), count: width)
+    for c in stride(from: 0, through: width - 1, by: 1) {
+        for r in stride(from: 0, through: height - 1, by: 1) {
+            transposed[c][r] = array[r][c]
+        }
+    }
+
+    return transposed
+}
