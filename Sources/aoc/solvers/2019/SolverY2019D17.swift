98643701f66dd601963caf6bc5120d6ba09a08d3
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2022-06-14 20:20:37 +0100

    Add 2019 day 17 part 2 solution

--- Sources/aoc/solvers/2019/SolverY2019D17.swift
+++ Sources/aoc/solvers/2019/SolverY2019D17.swift
@@ -56,6 +56,10 @@ fileprivate extension Coordinate {
     }
 }
 
+fileprivate enum Command : String {
+    case RotateLeft = "L", RotateRight = "R", StepForward = "1"
+}
+
 class SolverY2019D17 : Solvable {
     static var description = "Set and Forget"
 
@@ -91,6 +95,50 @@ class SolverY2019D17 : Solvable {
                 segments.max { a, b in a.y < b.y }!.y
             )
         )
+    }
+
+    fileprivate func convert(_ commands: [Command]) -> [String] {
+        return commands.compactMap { cmd in cmd.rawValue }
+            .joined()
+            .splitOnNewCharacter()
+            .compactMap { a in "\(a.first!)" == Command.StepForward.rawValue ? "\(a.count)" : "\(a.first!)" }
+    }
+
+    fileprivate func computeWalkInstructions(_ startDirection: Direction,
+                                             _ startCoord: Coordinate,
+                                             _ segments: [Coordinate],
+                                             _ log: Log) -> [String] {
+        var cmds = [Command]()
+        var cPos = startCoord
+        var cDir = startDirection
+
+        repeat {
+            var nPos = cPos
+            var nDir = cDir
+            let tPos = cPos.ifMove(cDir)
+            let lRot = cDir.left()
+            let rRot = cDir.right()
+
+            if segments.contains(tPos) {
+                nPos = tPos
+                cmds.append(.StepForward)
+            } else {
+                if segments.contains(cPos.ifMove(lRot)) {
+                    nDir = lRot
+                    cmds.append(.RotateLeft)
+                } else if segments.contains(cPos.ifMove(rRot)) {
+                    nDir = rRot
+                    cmds.append(.RotateRight)
+                } else {
+                    log.error(theMessage: "The was nowhere to go?")
+                }
+            }
+
+            cPos = nPos
+            cDir = nDir
+        } while cmds.filter { c in c == .StepForward }.count < segments.count
+
+        return convert(cmds)
     }
 
     fileprivate func getPathSegments(_ log: Log) -> (startDir: Direction, startCoord: Coordinate, segments: [Coordinate]) {
@@ -152,5 +200,70 @@ class SolverY2019D17 : Solvable {
     }
 
     func doPart2(withLog log: Log) {
+        let world = getPathSegments(log)
+        // the world segments need to be doubled up where they intersect.
+        let complete = world.segments + getIntersections(world.segments)
+        let instructions = computeWalkInstructions(world.startDir, world.startCoord, complete, log)
+
+        // I cheated here and worked out the groups by hand. They are:
+        let subA = ["L", "4", "L", "4", "L", "10", "R", "4"],
+            subB = ["R", "4", "L", "4", "L", "4", "R", "8", "R", "10"],
+            subC = ["R", "4", "L", "10", "R", "10"]
+        let main = instructions.joined(separator: ",")
+            .replacingOccurrences(of: subA.joined(separator: ","), with: "A")
+            .replacingOccurrences(of: subB.joined(separator: ","), with: "B")
+            .replacingOccurrences(of: subC.joined(separator: ","), with: "C")
+
+        // Print to log since it's helpful.
+        log.debug(theMessage: """
+            -- INFO --
+            The walk produced the following instructions:
+            \(instructions)
+            This breaks down into the following groups:
+            A = \(subA)
+            B = \(subB)
+            C = \(subC)
+            Producing the application:
+            MAIN = \(main)
+            """)
+
+        // Construct the application
+        var application = ""
+        application += main + "\n"                          // MAIN
+        application += subA.joined(separator: ",") + "\n"   // A
+        application += subB.joined(separator: ",") + "\n"   // B
+        application += subC.joined(separator: ",") + "\n"   // C
+        application += "n\n"                                // NO VIDEO
+
+        // Prpeare the robot
+        let ascii = Functions.sToA(application)
+        let vm = IntcodeVM(log: log, program: program)
+        ascii.forEach { char in vm.push(char) }
+        vm.memset(0, 2)
+
+        var vmStdout = [Int]()
+        loop: while (true) {
+            do {
+                try vm.run()
+                break loop
+            } catch IntcodeStatus.ioInterruptWrite {
+                if let d = try? vm.pop() {
+                    if d > UInt8.max {
+                        // success, clear the display.
+                        vmStdout.removeAll()
+                    }
+                    vmStdout.append(d)
+                }
+            } catch let error {
+                log.error(theMessage: "\(error)")
+                break loop
+            }
+        }
+
+        if vmStdout.count > 1 {
+            log.error(theMessage: Functions.aToS(vmStdout))
+        } else {
+            log.solution(theMessage: "The amount of space dust vacuumed is \(vmStdout.first!).")
+        }
     }
 }
