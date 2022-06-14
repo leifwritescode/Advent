f0e54dc1f28268c644ed2b7261cc5c5c94d55b14
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2022-06-14 20:20:23 +0100

    Group class extensions together

--- Sources/aoc/solvers/2015/SolverY2015D6.swift
+++ Sources/aoc/solvers/2015/SolverY2015D6.swift
@@ -104,23 +104,3 @@ class SolverY2015D6 : Solvable {
         }
     }
 }
-
-fileprivate extension String {
-    func groups(for regexPattern: String) throws -> [String] {
-        let regex = try NSRegularExpression(pattern: regexPattern)
-        let matches = regex.matches(in: self, range: NSRange(self.startIndex..., in: self))
-        
-        if (matches.isEmpty) {
-            return []
-        }
-        
-        let match = matches[0]
-        return (1..<match.numberOfRanges).map {
-            let rangeBounds = match.range(at: $0)
-            guard let range = Range(rangeBounds, in: self) else {
-                return ""
-            }
-            return String(self[range])
-        }
-    }
-}
