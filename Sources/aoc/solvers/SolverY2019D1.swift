ee91989f6bf31ef6dc9ffce68f6936e664c41a46
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2022-06-14 20:20:16 +0100

    Add 2019 day 1 solution

---
+++ Sources/aoc/solvers/SolverY2019D1.swift
@@ -0,0 +1,45 @@
+//
+//  SolverY2019D1.swift
+//  aoc
+//
+//  Created by Leif Walker-Grant on 18/10/2020.
+//
+
+import Foundation
+
+class SolverY2019D1 : Solvable {
+    var masses: [Int] = Array()
+    
+    required init (withLog log: Log, andInput input: String) {
+        input.components(separatedBy: .newlines).forEach {
+            masses.append(Int($0) ?? 0)
+        }
+    }
+    
+    private func simpleFuelFor(mass m: Int) -> Int {
+        return max(0, m / 3 - 2)
+    }
+
+    private func recursiveFuelFor(mass m: Int) -> Int {
+        let rm = simpleFuelFor(mass: m)
+        return rm > 0 ? rm + recursiveFuelFor(mass: rm) : rm
+    }
+    
+    func doPart1(withLog log: Log) -> Void {
+        var totalFuel = 0
+        masses.forEach {
+            totalFuel += simpleFuelFor(mass: $0)
+            log.debug(theMessage: "Interim mass is \(totalFuel).")
+        }
+        log.solution(theMessage: "The sum of the fuel requirements for all modules is \(totalFuel) units.")
+    }
+
+    func doPart2(withLog log: Log) -> Void {
+        var totalFuel = 0
+        masses.forEach {
+            totalFuel += recursiveFuelFor(mass: $0)
+            log.debug(theMessage: "Interim mass is \(totalFuel).")
+        }
+        log.solution(theMessage: "The sum of the fuel requirements, accounting for fuel mass, is \(totalFuel) units.")
+    }
+}
