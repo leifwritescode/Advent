aaa5acfc689348923d3081a34ae669b6ea39d980
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2022-06-14 20:20:20 +0100

    Add option to run one half of a solver only

--- Sources/aoc/main.swift
+++ Sources/aoc/main.swift
@@ -14,22 +14,32 @@ let minYear = 2015
 let maxYear = 2020
 
 struct Aoc: ParsableCommand {
-    @Flag(name: .shortAndLong, help: "Include additional debug information in puzzle solver output.")
-    var verbose = false
-
     @Option(name: .shortAndLong, help: "The year from which to run a puzzle solver, between \(minYear) and \(maxYear) inclusive.")
     var year: Int
 
     @Option(name: .shortAndLong, help: "The day for which to run the puzzle solver, between \(minDay) and \(maxDay) inclusive.")
     var day: Int
 
-    func validate() throws {
+    @Flag(name: .shortAndLong, help: "Include additional debug information in puzzle solver output.")
+    var verbose = false
+
+    @Flag(name: .shortAndLong, help: "Run the first half of the solution only.")
+    var first: Bool = false
+
+    @Flag(name: .shortAndLong, help: "Run the second half of the solution only.")
+    var second: Bool = false
+
+    mutating func validate() throws {
         guard Array(minYear...maxYear).contains(year) else {
             throw ValidationError("'<year>' must be between \(minYear) and \(maxYear), inclusive.")
         }
 
         guard Array(minDay...maxDay).contains(day) else {
             throw ValidationError("'<day>' must be between \(minDay) and \(maxDay), inclusive.");
+        }
+
+        guard !(first && second) else {
+            throw ValidationError("[--first] and [--second] are mutually exclusive.")
         }
     }
 
@@ -49,8 +59,14 @@ struct Aoc: ParsableCommand {
         }
 
         let solver = cls.init(withLog: log, andInput: input)
-        solver.doPart1(withLog: log)
-        solver.doPart2(withLog: log)
+
+        if (first || !second) {
+            solver.doPart1(withLog: log)
+        }
+
+        if (second || !first) {
+            solver.doPart2(withLog: log)
+        }
     }
 }
 
