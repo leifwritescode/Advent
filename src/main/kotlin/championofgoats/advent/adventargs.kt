e2f599cba0b9531a9b7ab1f96164b4182de9ad67
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2022-06-14 20:20:07 +0100

    Reintroduce kotlin-argparser support
    Maybe I should resist gradle less...

--- src/main/kotlin/championofgoats/advent/adventargs.kt
+++ src/main/kotlin/championofgoats/advent/adventargs.kt
@@ -1,26 +1,18 @@
 package championofgoats.advent
 
-class AdventArgs() {
+import com.xenomachina.argparser.*
 
-    var year: Int = 2019
-    var day: Int = 1
-    var inputDir: String = "src/championofgoats/advent/2019/data"
-    var outputDir: String = "src/championofgoats/advent/2019/day1"
-
-    fun parse(args: Array<String>) : Boolean {
-        if (args.size == 0 || args.size % 2 != 0)
-            return false
-
-        for (i in 0..args.size-1) {
-            var cur = args[i]
-            when (cur) {
-                "-y" -> year = args[i+1].toInt()
-                "-d" -> day = args[i+1].toInt()
-                "-i" -> inputDir = args[i+1]
-                "-o" -> outputDir = args[i+1]
-            }
-        }
-
-        return true
-    }
+class AdventArgs(parser: ArgParser) {
+    val year by parser.storing(
+        "-y", "--year", help = "the year from which to run solutions"
+    ) { toInt() } .default(2019)
+    val day by parser.storing(
+        "-d", "--day", help = "the solution to run"
+    ) { toInt() } .default(1)
+    val inputDir by parser.storing(
+        "-i", "--inputDir", help = "the directory from which to source inputs"
+    ) .default("src/main/kotlin/championofgoats/advent/2019/data")
+    val outputDir by parser.storing(
+        "-o", "--outputDir", help = "the directory in which to store outputs"
+    ) .default("src/main/kotlin/championofgoats/advent/2019/day1")
 }
