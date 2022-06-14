800aebd7b2f88cf663e63e379c832123482d2cfb
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2022-06-14 20:20:06 +0100

    Solutions are derived from problems
    Make each solution a derived class of problem
    Each solution takes as parametrs:
      - an input directory (data)
      - an output directory (day*)
      - and a logger instance

--- src/championofgoats/advent/2019/day1/day1.kt
+++ src/championofgoats/advent/2019/day1/day1.kt
@@ -1,36 +1,48 @@
 package championofgoats.advent.twentynineteen.day1
 
 import java.io.File
+import championofgoats.advent.Problem
+import championofgoats.advent.utils.logging.Logger
+import championofgoats.advent.utils.logging.ConsoleLogger
 
-// fuel for module of mass m is (m/3)-2
-fun getFuel(mass: Int) : Int =  (mass / 3) - 2
+object Day1 : Problem {
+    override fun solve(inputDir: String, outputDir: String, log: Logger)  {
+        // fuel for module of mass m is (m/3)-2
+        fun getFuel(mass: Int) : Int =  (mass / 3) - 2
+        
+        // fuel for module of mass m is (m/3)-2
+        // this function recursively computes the fuel required to carry the fuel
+        // the minimum value of m for which (m/3)-2 > 0 is 9, which will be our break value
+        fun getFuelRecursive(mass: Int) : Int {
+            if (mass < 9)
+                return 0
+        
+            var m = (mass / 3) - 2
+            return m + getFuelRecursive(m)
+        }
 
-// fuel for module of mass m is (m/3)-2
-// this function recursively computes the fuel required to carry the fuel
-// the minimum value of m for which (m/3)-2 > 0 is 9, which will be our break value
-fun getFuelRecursive(mass: Int) : Int {
-    if (mass < 9)
-        return 0
-
-    var m = (mass / 3) - 2
-    return m + getFuelRecursive(m)
+        //if (args.size != 1) {
+        //    println("Usage: java -jar day1.jar <input file>")
+        //    return
+        //}
+    
+        var fuelNeededPartA = 0
+        var fuelNeededPartB = 0;
+    
+        // we won't bother checking that the file exists
+        File("$inputDir/day1.input").forEachLine {
+            fuelNeededPartA += getFuel(it.toInt())
+            fuelNeededPartB += getFuelRecursive(it.toInt())
+        }
+    
+        log.Solution("DAY1p1 ans = %d".format(fuelNeededPartA))
+        log.Solution("DAY1p2 ans = %d".format(fuelNeededPartB))
+    }
 }
 
 fun main(args: Array<String>) {
-    if (args.size != 1) {
-        println("Usage: java -jar day1.jar <input file>")
-        return
-    }
-
-    var fuelNeededPartA = 0
-    var fuelNeededPartB = 0;
-
-    // we won't bother checking that the file exists
-    File(args[0]).forEachLine {
-        fuelNeededPartA += getFuel(it.toInt())
-        fuelNeededPartB += getFuelRecursive(it.toInt())
-    }
-
-    println("DAY1p1 ans = %d".format(fuelNeededPartA))
-    println("DAY1p2 ans = %d".format(fuelNeededPartB))
+    Day1.solve(
+        "src/championofgoats/advent/2019/data",
+        "src/championofgoats/advent/2019/day1",
+        ConsoleLogger())
 }
--- src/championofgoats/advent/2019/day4/day4.kt
+++ src/championofgoats/advent/2019/day4/day4.kt
@@ -2,52 +2,59 @@ package championofgoats.advent.twentynineteen.day4
 
 import java.io.File
 import kotlin.text.Regex
+import championofgoats.advent.Problem
+import championofgoats.advent.utils.logging.Logger
+import championofgoats.advent.utils.logging.ConsoleLogger
+
+object Day4 : Problem {
+    override fun solve(inputDir: String, outputDir: String, log: Logger)  {
+        var day4p1ans = 0
+        var day4p2ans = 0
+        
+        var lowBound = 0
+        var highBound = 0
+        File("$inputDir/day4.input").forEachLine { 
+            var range = it.split("-")
+            lowBound = range[0].toInt()
+            highBound = range[1].toInt()
+        }
+        
+        // this regex finds pairs of numbers
+        var digitPairTest = "(\\d)\\1".toRegex()
+        for (i in lowBound..highBound) {
+            var cand = i.toString()
+            if (digitPairTest.containsMatchIn(cand)) {
+                // so if it matches: split it, sort it, join it
+                // if the results match, we've got a success!
+                var sortTest = (fun (str: String) : String { var toSort = str.toCharArray(); toSort.sort(); return toSort.joinToString(""); })(cand)
+                if (cand == sortTest)
+                day4p1ans += 1
+            }
+        }
+        
+        // this regex finds 
+        digitPairTest = "(\\d)(\\1+)".toRegex()
+        for (i in lowBound..highBound) {
+            var cand = i.toString()
+            // find all matches, map down to group value, drop the first group (we don't care about the full match)
+            // if the digit appears twice, and only twice, it's a pass
+            if (digitPairTest.findAll(cand).any { it.groups.drop(1).mapNotNull { m -> m?.value }.joinToString("").length == 2 }) {
+                // this step is the same as for p1
+                // if the results match, we've got a success!
+                var sortTest = (fun (str: String) : String { var toSort = str.toCharArray(); toSort.sort(); return toSort.joinToString(""); })(cand)
+                if (cand == sortTest)
+                day4p2ans += 1
+            }
+        }
+        
+        log.Solution("DAY4p1 ans = %d".format(day4p1ans))
+        log.Solution("DAY4p2 ans = %d".format(day4p2ans))
+    }
+}
 
 fun main(args: Array<String>) {
-    if (args.size != 1) {
-        println("Usage: java -jar day4.jar <input file>")
-        return
-    }
-
-    var day4p1ans = 0
-    var day4p2ans = 0
-
-    var lowBound = 0
-    var highBound = 0
-    File(args[0]).forEachLine { 
-        var range = it.split("-")
-        lowBound = range[0].toInt()
-        highBound = range[1].toInt()
-    }
-    
-    // this regex finds pairs of numbers
-    var digitPairTest = "(\\d)\\1".toRegex()
-    for (i in lowBound..highBound) {
-        var cand = i.toString()
-        if (digitPairTest.containsMatchIn(cand)) {
-            // so if it matches: split it, sort it, join it
-            // if the results match, we've got a success!
-            var sortTest = (fun (str: String) : String { var toSort = str.toCharArray(); toSort.sort(); return toSort.joinToString(""); })(cand)
-            if (cand == sortTest)
-                day4p1ans += 1
-        }
-    }
-
-    // this regex finds 
-    digitPairTest = "(\\d)(\\1+)".toRegex()
-    for (i in lowBound..highBound) {
-        var cand = i.toString()
-        // find all matches, map down to group value, drop the first group (we don't care about the full match)
-        // if the digit appears twice, and only twice, it's a pass
-        if (digitPairTest.findAll(cand).any { it.groups.drop(1).mapNotNull { m -> m?.value }.joinToString("").length == 2 }) {
-            // this step is the same as for p1
-            // if the results match, we've got a success!
-            var sortTest = (fun (str: String) : String { var toSort = str.toCharArray(); toSort.sort(); return toSort.joinToString(""); })(cand)
-            if (cand == sortTest)
-                day4p2ans += 1
-        }
-    }
-
-    println("DAY4p1 ans = %d".format(day4p1ans))
-    println("DAY4p2 ans = %d".format(day4p2ans))
+    Day4.solve(
+    "src/championofgoats/advent/2019/data",
+    "src/championofgoats/advent/2019/day4",
+    ConsoleLogger())
 }
