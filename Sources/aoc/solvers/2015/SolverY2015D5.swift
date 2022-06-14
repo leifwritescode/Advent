4b1fe4de2a6bfac22bea3e90e1a973e48ae807c1
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2022-06-14 20:20:22 +0100

    Add 2015 day 5 solution

---
+++ Sources/aoc/solvers/2015/SolverY2015D5.swift
@@ -0,0 +1,60 @@
+//
+//  SolverY2015D5.swift
+//  aoc
+//
+//  Created by Leif Walker-Grant on 24/10/2020.
+//
+
+import Foundation
+
+class SolverY2015D5 : Solvable {
+    let candidates: [String]
+
+    required init(withLog log: Log, andInput input: String) {
+        candidates = input.components(separatedBy: .newlines)
+    }
+
+    func doPart1(withLog log: Log) {
+        _ = timed(toLog: log) {
+            let niceStrings = candidates.filter { s in
+                var nice = false
+                if (!s.matches(#"([aeiou].*){3}"#)) {
+                    log.debug(theMessage: "\(s) violates first rule.")
+                }  else if (!s.matches(#"(\w)\1"#)) {
+                    log.debug(theMessage: "\(s) violates second rule.")
+                } else if (!s.matches(#"^((?!ab|cd|pq|xy)\w)*$"#)) {
+                    log.debug(theMessage: "\(s) violates third rule.")
+                } else {
+                    nice = true
+                }
+                return nice
+            }
+
+            log.solution(theMessage: "Of those given, \(niceStrings.count) strings are nice.")
+        }
+    }
+
+    func doPart2(withLog log: Log) {
+        _ = timed(toLog: log) {
+            let niceStrings = candidates.filter { s in
+                var nice = false
+                if (!s.matches(#"(\w\w)(?:.*)(\1)"#)) {
+                    log.debug(theMessage: "\(s) violates first rule.")
+                } else if (!s.matches(#"(\w)(?:.)(\1)"#)) {
+                    log.debug(theMessage: "\(s) violates second rule.")
+                } else {
+                    nice = true
+                }
+                return nice
+            }
+
+            log.solution(theMessage: "Of those given, applying the alternate rules, \(niceStrings.count) strings are nice.")
+        }
+    }
+}
+
+extension String {
+    func matches(_ regex: String) -> Bool {
+        return self.range(of: regex, options: .regularExpression) != nil
+    }
+}
