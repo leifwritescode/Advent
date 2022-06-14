9801d437d46ab7df0a9aa7d8cc0c6cbbfdbf3b1b
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2022-06-14 20:20:27 +0100

    Add 2016 day 6 solution

---
+++ Sources/aoc/solvers/2016/SolverY2016D6.swift
@@ -0,0 +1,60 @@
+//
+//  SolverY2016D6.swift
+//  aoc
+//
+//  Created by Leif Walker-Grant on 29/10/2020.
+//
+
+import Foundation
+
+class SolverY2016D6 : Solvable {
+    let signals: [[String?]]
+
+    required init(withLog log: Log, andInput input: String) {
+        let temp = input.components(separatedBy: .newlines)
+            .compactMap { s in
+                s.map { c in
+                    String(c)
+                }
+            }
+        signals = transpose2d(array: temp, width: temp[0].count, height: temp.count)
+    }
+
+    func doPart1(withLog log: Log) {
+        _ = timed(toLog: log) {
+            let ecMessage = signals.compactMap { line in
+                let next = line.compactMap { s in s  }
+                    .sorted()
+                    .joined()
+                    .splitOnNewCharacter()
+                    .sorted { a, b in
+                        a.count > b.count
+                    }
+                    .first!
+                    .first!
+                return String(next)
+            }.joined()
+
+            log.solution(theMessage: "The error-corrected SRC message is '\(ecMessage).'")
+        }
+    }
+
+    func doPart2(withLog log: Log) {
+        _ = timed(toLog: log) {
+            let ecMessage = signals.compactMap { line in
+                let next = line.compactMap { s in s  }
+                    .sorted()
+                    .joined()
+                    .splitOnNewCharacter()
+                    .sorted { a, b in
+                        a.count > b.count
+                    }
+                    .last!
+                    .first!
+                return String(next)
+            }.joined()
+
+            log.solution(theMessage: "The error-corrected MRC message is '\(ecMessage).'")
+        }
+    }
+}
