a516cf1006eefc0d435a448e341f2243d292494a
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2022-06-14 20:20:08 +0100

    Find solutions by reflection

--- src/main/kotlin/championofgoats/advent/main.kt
+++ src/main/kotlin/championofgoats/advent/main.kt
@@ -2,35 +2,21 @@ package championofgoats.advent
 
 import com.xenomachina.argparser.*
 import championofgoats.advent.AdventArgs
+import championofgoats.advent.Mirror
 import championofgoats.advent.utils.logging.ConsoleLogger
-import championofgoats.advent.twentynineteen.day1.Day1
-import championofgoats.advent.twentynineteen.day2.Day2
-import championofgoats.advent.twentynineteen.day3.Day3
-import championofgoats.advent.twentynineteen.day4.Day4
-import championofgoats.advent.twentynineteen.day5.Day5
-import championofgoats.advent.twentynineteen.day6.Day6
-import championofgoats.advent.twentynineteen.day7.Day7
-import championofgoats.advent.twentynineteen.day8.Day8
-
-val problems = hashMapOf<Int, Array<Problem>>(
-Pair(2019, arrayOf<Problem>(Day1, Day2, Day3, Day4,
-Day5, Day6, Day7, Day8))
-)
-
 
 fun main(args: Array<String>) = mainBody {
+    val log = ConsoleLogger()
     ArgParser(args).parseInto(::AdventArgs).run {
-        val log = ConsoleLogger()
-        val problemsForYear = problems.getOrDefault(year, emptyArray())
-        if (!problemsForYear.isNullOrEmpty()) {
-            val problem = problemsForYear.getOrNull(day - 1)
-            if (problem != null) {
-                problem.solve(inputDir, outputDir, log)
-            } else {
-                log.Error("No solution for %d/12/%d, exiting.".format(day, year))
+        Mirror.findProblemsFor(year, log)?.apply {
+            getOrNull(day - 1)?.apply {
+                solve(inputDir, outputDir, log)
+            }.also {
+                it ?: log.Error("No solution object Day$day was found, exiting.")
             }
-        } else {
-            log.Error("No solutions for %d, exiting.".format(year))
+        }.also {
+            it ?: log.Error("No package was found matching year $year, exiting.")
         }
     }
+    log.Debug("Finished")
 }
--- src/main/kotlin/championofgoats/advent/mirror.kt
+++ src/main/kotlin/championofgoats/advent/mirror.kt
@@ -1,29 +1,30 @@
 package championofgoats.advent
-/*
-import java.lang.reflect.*
-//import org.reflections.Reflections
+
+import org.reflections.Reflections
 import championofgoats.advent.Problem
 import championofgoats.advent.utils.logging.Logger
 
 object Mirror {
     private val years = hashMapOf(
-        Pair(2019, "twentynineteen"))
+    Pair(2019, "twentynineteen"))
     
     fun findProblemsFor(year: Int, log: Logger) : List<Problem>? {
         if (!years.containsKey(year)) {
-            log.Error("Key %s was not found.".format(year))
+            log.Warning("No package was found matching year %s.".format(year))
             return null
         }
-
-        val rgx = "championofgoats.advent.%s.day[0-9]{2}".format(years[year]).toRegex()
-        //val reflector = Reflections("championofgoats.advent.%s".format(years[year]))
-
-        championofgoats.advent.Problem::class.sealedSubclasses
-
-        //return reflector.getSubTypesOf(Problem::class.java)
-        //    .filter { it.name.matches(rgx) }
-        //    .mapNotNull { it.kotlin.objectInstance }
-        //    .sortedBy { it.javaClass.simpleName }
+        
+        val rgx = "championofgoats\\.advent\\.%s\\.day[0-9]{1,2}\\.Day[0-9]{1,2}".format(years[year]).toRegex()
+        val reflector = Reflections("championofgoats.advent.%s".format(years[year]))
+        return reflector.getSubTypesOf(Problem::class.java)
+        .filter {
+            it.name.matches(rgx)
+        }
+        .mapNotNull {
+            it.kotlin.objectInstance
+        }
+        .sortedBy {
+            it.javaClass.simpleName
+        }
     }
 }
- */
