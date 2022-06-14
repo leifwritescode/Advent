688ebd47856da8d32994ccc858d80cd5bc2e8824
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2022-06-14 20:20:05 +0100

    Add warning/error/solution methods to logger

--- src/championofgoats/advent/utils/logging/consolelogger.kt
+++ src/championofgoats/advent/utils/logging/consolelogger.kt
@@ -10,4 +10,16 @@ class ConsoleLogger : Logger {
     override fun Info(m: String) {
         println("[INF] %s".format(m))
     }
+
+    override fun Warning(m: String) {
+        println("%c[33m[WRN] %s%c[0m".format(27, m, 27))
+    }
+
+    override fun Error(m: String) {
+        println("%c[31m[ERR] %s%c[0m".format(27, m, 27))
+    }
+
+    override fun Solution(m: String) {
+        println("%c[32m[SLV] %s%c[0m".format(27, m, 27))
+    }
 }
--- src/championofgoats/advent/utils/logging/nulllogger.kt
+++ src/championofgoats/advent/utils/logging/nulllogger.kt
@@ -3,6 +3,9 @@ package championofgoats.advent.utils.logging
 class NullLogger : Logger {
     override fun Debug(m: String) { }
     override fun Info(m: String) { }
+    override fun Warning(m: String) { }
+    override fun Error(m: String) { }
+    override fun Solution(m: String) { }
 }
 
 fun Logger?.NotNull(): Logger = if (this == null) NullLogger() else this
