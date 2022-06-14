66608f192102286400233afab93f5ecd75df3e4e
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2022-06-14 20:20:21 +0100

    In SolverY2015D4, chunk using GCD to improve efficiency.
    With non-optimised parameters, solution runs ~5x faster than the naive implemention.

--- Sources/aoc/solvers/2015/SolverY2015D4.swift
+++ Sources/aoc/solvers/2015/SolverY2015D4.swift
@@ -9,28 +9,67 @@ import Foundation
 import CryptoKit
 
 class SolverY2015D4 : Solvable {
-    let secretKey: String
+    private let sema: DispatchSemaphore
+    private let secretKey: String
+
+    private let cToken: CancellationToken = CancellationToken()
+    private let atomic = DispatchQueue(label: "Suffix")
+    private let queue = DispatchQueue(label: "Solver", attributes: .concurrent)
+    private let group = DispatchGroup()
+    @Atomic private var suffix: Int = Int.max
 
     required init(withLog log: Log, andInput input: String) {
         secretKey = input.trimmingCharacters(in: CharacterSet.newlines)
+        sema = DispatchSemaphore(value: ProcessInfo().activeProcessorCount)
     }
 
-    private func doTask(withPrefix prefix: String, andLog log: Log) {
-        if #available(OSX 10.15, *) {
-            var found: Bool = false
-            var sk2 = 1
-            repeat {
-                let digest = (secretKey + String(sk2)).MD5
-                found = digest.starts(with: prefix)
-                if (found) {
-                    log.solution(theMessage: "The lowest possible number to produce a digest beginning with \(prefix) is \(sk2).")
-                } else {
-                    sk2 += 1
-                    if (sk2 % 100000 == 0) {
-                        log.debug(theMessage: "Sk2 hit \(sk2).")
+    private func doTaskQueued(prefix: String, log: Log, start: Int, offset: Int) {
+        log.debug(theMessage: "Spawning thread for range \(start) ... \(start + offset).")
+
+        queue.async {
+            self.group.enter()
+
+            if (!self.cToken.isCancelled) {
+                for value in start ... (start + offset) {
+                    let digest = (self.secretKey + String(value)).MD5
+                    if (digest.starts(with: prefix)) {
+                        self.cToken.cancel()
+                        self.suffix = value
+                        break
                     }
                 }
-            } while !found
+                log.debug(theMessage: "Finished testing range \(start) ... \(start + offset).")
+            } else {
+                log.error(theMessage: "Cancelling thread spawn for range \(start) ... \(start + offset).")
+            }
+
+            self.sema.signal()
+            self.group.leave()
+        }
+    }
+
+    private func doTask(withPrefix prefix: String, andLog log: Log, searchRange: Int) {
+        if #available(OSX 10.15, *) {
+            var currOffset = 1
+
+            suffix = Int.max
+            cToken.reset()
+
+            repeat {
+                sema.wait()
+
+                doTaskQueued(prefix: prefix,
+                             log: log,
+                             start: currOffset,
+                             offset: searchRange)
+
+                currOffset += searchRange
+            } while !cToken.isCancelled
+
+            log.info(theMessage: "A solution is available. Waiting for remaining solvers to finish, as a smaller solution may be found.")
+            group.wait()
+
+            log.solution(theMessage: "The lowest possible number to produce a digest beginning with \(prefix) is \(suffix).")
         } else {
             log.error(theMessage: "SolverY2015D4 cannot be run on macOS < 10.15.")
         }
@@ -38,13 +77,13 @@ class SolverY2015D4 : Solvable {
 
     func doPart1(withLog log: Log) {
         _ = timed(toLog: log) {
-            doTask(withPrefix: "00000", andLog: log)
+            doTask(withPrefix: "00000", andLog: log, searchRange: 10000)
         }
     }
 
     func doPart2(withLog log: Log) {
         _ = timed(toLog: log) {
-            doTask(withPrefix: "000000", andLog: log)
+            doTask(withPrefix: "000000", andLog: log, searchRange: 10000)
         }
     }
 }
