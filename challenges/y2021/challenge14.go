edd9e95d6c397688583c39dabf1923b83428095a
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2022-06-14 20:19:53 +0100

    Remove unused code from 2021-14

--- challenges/y2021/challenge14.go
+++ challenges/y2021/challenge14.go
@@ -3,7 +3,6 @@ package challenges
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"math"
 	"strings"
 
@@ -32,50 +31,6 @@ func (c *Challenge14) Initialise(file_path string) error {
 		}
 	}
 	return err
-}
-
-func (c *Challenge14) generateNewPolymer(input string) string {
-	var output = ""
-	runes := []rune(input)
-	for i := 0; i < len(runes); i++ {
-		if i+1 >= len(runes) {
-			output = fmt.Sprintf("%s%c", output, runes[i])
-		} else {
-			a := runes[i]
-			b := runes[i+1]
-			d := fmt.Sprintf("%c%c", a, b)
-			e := c.compounds[d]
-			output = fmt.Sprintf("%s%c%s", output, a, e)
-		}
-	}
-	return output
-}
-
-// Because the string grows to len*2-1 each time,
-// It is ultimately 2^cycles*(init_len-1)+1 in length
-func (c *Challenge14) solve(cycles int) string {
-	// compute the polymer
-	s := c.formula
-	for i := 0; i < cycles; i++ {
-		log.Println("Starting cycle", i+1)
-		s = c.generateNewPolymer(s)
-	}
-
-	// map the compounds to their occurrences
-	counts := make(map[rune]int)
-	for _, r := range s {
-		counts[r]++
-	}
-
-	// find the answer
-	min := math.MaxUint32
-	max := 0
-	for _, v := range counts {
-		min = common_math.Min32(min, v)
-		max = common_math.Max32(max, v)
-	}
-
-	return fmt.Sprintf("%d", max-min)
 }
 
 func (c *Challenge14) solve2(cycles int) string {
