8734dfb6d2d9eb412f0a2c1d3cbf7e6263c3c1f4
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2022-06-14 20:20:48 +0100

    HashMap-based optimisation for 2020 day 15, part 2
    Store associative array of values and their last known index.
    This drops run time to ~30s from 4 hours.

--- Sources/aoclib/solvers/2020/SolverY2020D15.swift
+++ Sources/aoclib/solvers/2020/SolverY2020D15.swift
@@ -29,11 +29,31 @@ class SolverY2020D15 : Solvable {
         return state.last!
     }
 
+    func recite2(_ till: Int, _ log: Log) -> Int {
+        // set up a dictionary of each value (the key) and the last time it was seen (value)
+        var map = [Int:Int]()
+        initial.enumerated().forEach { e in map[e.element] = e.offset + 1 }
+        // get the first test value
+        var next = map.key(forValue: initial.count)!
+        // loop until we've computed the final case
+        for term in initial.count..<till {
+            // get the delta between now and the last time we saw next, or 0 if we haven't
+            let delta = term - map[next, default: term]
+            // the last time we saw next is in this cycle
+            // this looks counterintuitive, but keep in mind that the sequence is 1-indexed.
+            map[next] = term
+            // delta is now the last value we saw
+            next = delta
+        }
+        // the final value of next is our solution
+        return next
+    }
+
     func doPart1(withLog log: Log) {
         log.solution(theMessage: "The 2020th number in the sequence is \(recite(2020, log)).")
     }
 
     func doPart2(withLog log: Log) {
-        log.solution(theMessage: "The 30 millionth term is \(recite(30000000, log)).")
+        log.solution(theMessage: "The 30 millionth term is \(recite2(30000000, log)).")
     }
 }
--- Tests/aoc-test/2020/TestSolverY2020D15.swift
+++ Tests/aoc-test/2020/TestSolverY2020D15.swift
@@ -14,7 +14,8 @@ class TestSolverY2020D15: XCTestCase {
                 using: """
                 0,3,6
                 """,
-                expecting: "436")
+                expecting: "436",
+                and: "175594")
     }
 
     func test_ka__e2_p1p2() throws {
@@ -22,7 +23,8 @@ class TestSolverY2020D15: XCTestCase {
                 using: """
                 1,3,2
                 """,
-                expecting: "1")
+                expecting: "1",
+                and: "2578")
     }
 
     func test_ka__e3_p1p2() throws {
@@ -30,7 +32,8 @@ class TestSolverY2020D15: XCTestCase {
                 using: """
                 2,1,3
                 """,
-                expecting: "10")
+                expecting: "10",
+                and: "3544142")
     }
 
     func test_ka__e4_p1p2() throws {
@@ -38,7 +41,8 @@ class TestSolverY2020D15: XCTestCase {
                 using: """
                 1,2,3
                 """,
-                expecting: "27")
+                expecting: "27",
+                and: "261214")
     }
 
     func test_ka__e5_p1p2() throws {
@@ -46,7 +50,8 @@ class TestSolverY2020D15: XCTestCase {
                 using: """
                 2,3,1
                 """,
-                expecting: "78")
+                expecting: "78",
+                and: "6895259")
     }
 
     func test_ka__e6_p1p2() throws {
@@ -54,7 +59,8 @@ class TestSolverY2020D15: XCTestCase {
                 using: """
                 3,2,1
                 """,
-                expecting: "438")
+                expecting: "438",
+                and: "18")
     }
 
     func test_ka__e7_p1p2() throws {
@@ -62,6 +68,7 @@ class TestSolverY2020D15: XCTestCase {
                 using: """
                 3,1,2
                 """,
-                expecting: "1836")
+                expecting: "1836",
+                and: "362")
     }
 }
