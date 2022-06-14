0e6a5ca0c5d1d4275e09ac08de46c8b3f9972686
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2022-06-14 20:20:05 +0100

    Add warning/error/solution methods to logger

--- src/championofgoats/advent/utils/logging/logger.kt
+++ src/championofgoats/advent/utils/logging/logger.kt
@@ -3,4 +3,7 @@ package championofgoats.advent.utils.logging
 interface Logger {
     fun Debug(m: String)
     fun Info(m: String)
+    fun Warning(m: String)
+    fun Error(m: String)
+    fun Solution(m: String)
 }
