4a03aed09b64c8e8018abeb58c0e1014d0769d2f
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2022-06-14 20:20:14 +0100

    Update utility classes and functions

--- src/main/kotlin/championofgoats/advent/utils/direction.kt
+++ src/main/kotlin/championofgoats/advent/utils/direction.kt
@@ -1,6 +1,15 @@
 package championofgoats.advent.utils
 
+import kotlin.Comparator
+import kotlin.comparisons.*
+
 // A structure representing the direction between two points
 // $sign is true if the slope is descending, else false
 // $nominator/$denominator are the slope
-data class Direction(var sign: Boolean, var nominator: Int, var denominator: Int)
+data class Direction(var sign: Boolean, var nominator: Int, var denominator: Int) {
+    companion object {
+        val comparator: Comparator<Direction> = compareBy<Direction> { d -> d.sign }
+        .thenBy { d -> d.denominator != 0 }
+        .thenComparator { (_, y1, x1), (_, y2, x2) -> compareValues(y1 * x2, y2 * x1) }
+    }
+}
--- src/main/kotlin/championofgoats/advent/utils/point.kt
+++ src/main/kotlin/championofgoats/advent/utils/point.kt
@@ -2,34 +2,31 @@ package championofgoats.advent.utils
 
 import kotlin.math.abs
 import kotlin.math.sign
-import championofgoats.advent.utils.gcd
-import championofgoats.advent.utils.Direction
 
 // A coordinate-space agnostic point in 2D space
-data class Point(var x: Int, var y: Int)
-
-// Returns the directon between this point and another
-fun Point.directionTo(other: Point): Direction {
-    var out: Direction
-    if (this.x == other.x) {
-        // the slope is horizontal
-        out = Direction(this.y < other.y, 1, 0)
-    } else {
-        // a / b is the slope
-        val a = other.y - this.y
-        val b = other.x - this.x
-
-        // gcd of (|a|, |b|) lets us simplify
-        val c = gcd(abs(a), abs(b))
-
-        // extract sign (either -1 or 1)
-        val s = (this.x - other.x).sign
-
-        // populate
-        out = Direction(
-        this.x > other.x, // true if negative
-        (a / c) * s, // (a / gcd(|a|, |b|)) * sign
-        (b / c) * s) // (b / gcd(|a|, |b|)) * sign
+data class Point(var x: Int, var y: Int) {
+    fun directionTo(other: Point): Direction {
+        var out: Direction
+        if (this.x == other.x) {
+            // the slope is horizontal
+            out = Direction(this.y < other.y, 1, 0)
+        } else {
+            // a / b is the slope
+            val a = other.y - this.y
+            val b = other.x - this.x
+            
+            // gcd of (|a|, |b|) lets us simplify
+            val c = gcd(abs(a), abs(b))
+            
+            // extract sign (either -1 or 1)
+            val s = (this.x - other.x).sign
+            
+            // populate
+            out = Direction(
+            this.x > other.x, // true if negative
+            (a / c) * s, // (a / gcd(|a|, |b|)) * sign
+            (b / c) * s) // (b / gcd(|a|, |b|)) * sign
+        }
+        return out
     }
-    return out
 }
