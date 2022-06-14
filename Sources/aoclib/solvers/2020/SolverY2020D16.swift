c3c4ef616387c88acf93ce157cc2cd16c8fb7e81
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2022-06-14 20:20:49 +0100

    Optimise 2020 day 2 part 2 solution
    Use sets for constraint storage.
    Cache the enumerated columns.
    
    Run time is now ~200ms, down from ~40s.

--- Sources/aoclib/solvers/2020/SolverY2020D16.swift
+++ Sources/aoclib/solvers/2020/SolverY2020D16.swift
@@ -10,7 +10,7 @@ import Foundation
 class SolverY2020D16 : Solvable {
     static var description = "Ticket Translation"
 
-    let ranges: [String:[Int]]
+    let ranges: [String:Set<Int>]
     let myTicket: [Int]
     let tickets: [[Int]]
 
@@ -18,10 +18,10 @@ class SolverY2020D16 : Solvable {
         var sections = input.components(separatedBy: .newlines).split(separator: "")
 
         ranges = sections.removeFirst()
-            .reduce([String:[Int]]()) { dict, line in
+            .reduce([String:Set<Int>]()) { dict, line in
             var dict = dict
             let groups = line.groups(for: #"([\w\s]+):\s(\d+)-(\d+)\sor\s(\d+)-(\d+)"#).first!
-            dict[groups[0]] = Array(Int(groups[1])!...Int(groups[2])!) + Array(Int(groups[3])!...Int(groups[4])!)
+            dict[groups[0]] = Set(Array(Int(groups[1])!...Int(groups[2])!) + Array(Int(groups[3])!...Int(groups[4])!))
             return dict
         }
 
@@ -45,15 +45,21 @@ class SolverY2020D16 : Solvable {
     }
 
     func doPart2(withLog log: Log) {
-        // Filter out tickets that have invalid fields
         log.info(theMessage: "Filtering invalid tickets...")
         let candidates = tickets.filter { ticket in
-            ticket.filter { value in ranges.allSatisfy { _, v in !v.contains(value) }}.count == 0
+            // Filter out any ticket with a value that satisfies no field constraints.
+            ticket.filter { value in ranges.allSatisfy { _, v in !v.contains(value) }}.isEmpty
         }
         log.info(theMessage: "Done!")
 
-        // Track the assigned fields in order of assignment.
+        // Track the assigned fields.
         var assigned = [(String, Int)]()
+
+        // Cache the column enumerators.
+        var columns = [EnumeratedSequence<[Int]>]()
+        for i in 0..<candidates.first!.count {
+            columns.append(candidates.reduce([Int]()) { r, a in r + [a[i]] }.enumerated())
+        }
 
         var cycle = 1
         outer: while assigned.count != ranges.count {
@@ -63,12 +69,11 @@ class SolverY2020D16 : Solvable {
             // Create a set of all columns, then subtract all assigned columns and iterate.
             let range = Set(0..<candidates.first!.count).subtracting(assigned.compactMap { $0.1 })
             inner: for i in range {
-                // Map the column to a single dimension and iterate until we exhaust it or break out.
-                let col = candidates.reduce([Int]()) { r, a in r + [a[i]] }
-                var iter = col.enumerated().makeIterator()
+                // Get an iterator from the cached columns
+                var iter = columns[i].makeIterator()
                 var curr = iter.next()
                 while curr != nil {
-                    // Find all potential fields for this row, and intersect with the unassigned rows.
+                    // Find all potential fields for this row, and intersect with the unassigned fields.
                     // The result is potential fields that all processed rows can be.
                     let potentials = ranges.filter { k, v in v.contains(curr!.element) }
                     unassigned = unassigned.intersection(potentials.keys)
@@ -79,10 +84,10 @@ class SolverY2020D16 : Solvable {
                         log.info(theMessage: "Finished. Position #\(i + 1) encodes \(unassigned.first!).")
                         assigned.append((unassigned.first!, i))
                         break inner
-                    } else {
-                        // Investigate the next row.
-                        curr = iter.next()
                     }
+
+                    // Investigate the next row.
+                    curr = iter.next()
                 }
             }
 
