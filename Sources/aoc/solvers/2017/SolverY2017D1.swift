78316335ed74e87177dab6b2de9a71b1f9abbd4d
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2022-06-14 20:20:29 +0100

    Add 2017 day 1 solution

---
+++ Sources/aoc/solvers/2017/SolverY2017D1.swift
@@ -0,0 +1,53 @@
+//
+//  SolverY2017D1.swift
+//  aoc
+//
+//  Created by Leif Walker-Grant on 10/11/2020.
+//
+
+import Foundation
+
+class SolverY2017D1 : Solvable {
+    let input: String
+
+    required init(withLog log: Log, andInput input: String) {
+        self.input = input
+    }
+
+    func doPart1(withLog log: Log) {
+        _ = timed(toLog: log) {
+            var checksum = input.splitOnNewCharacter()
+            if checksum.last?.first == checksum.first?.first {
+                let last = checksum.dropLast().first!
+                checksum[0].append(last)
+            }
+
+            var sum = 0
+            checksum.filter { s in
+                s.count > 1
+            }.forEach { s in
+                let num = Int("\(s.first!)")!
+                sum += num * (s.count - 1)
+            }
+
+            log.solution(theMessage: "The checksum is \(sum).")
+        }
+    }
+
+    func doPart2(withLog log: Log) {
+        _ = timed(toLog: log) {
+            let str = Array(input)
+            let halfcount = input.count / 2
+
+            var sum = 0
+            for (i, c) in str.enumerated() {
+                let next = (i + halfcount) % input.count
+                if c == str[next] {
+                    sum += Int("\(c)")!
+                }
+            }
+
+            log.solution(theMessage: "The checksum, using half-count, is \(sum).")
+        }
+    }
+}
