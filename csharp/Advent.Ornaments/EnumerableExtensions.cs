8b3f6993950a40f8ad1763a63ae201c2803ea63c
Leif <24778021+LeifWritesCode@users.noreply.github.com>
2022-12-08 20:23:12 +0100

    Implement AoC 2022 Day 8 (#2)

---
+++ csharp/Advent.Ornaments/EnumerableExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Advent.Ornaments;
+
+public static class EnumerableExtensions
+{
+    /// <summary>
+    /// Returns elements from a sequence as long as a specified condition is true.
+    /// </summary>
+    /// <typeparam name="Tvalue">The type of the elements of <paramref name="source"/>.</typeparam>
+    /// <param name="self">A sequence to return elements from.</param>
+    /// <param name="predicate">A function to test each element for a condition.</param>
+    /// <param name="inclusive">If true, includes the element at which the test no longer passes.</param>
+    /// <returns>
+    /// An <see cref="IEnumerable{T}"/> that contains the elements from the input sequence that occur up to and, if <paramref name="inclusive"/> is true, including the element at which the test no longer passes.
+    /// </returns>
+    public static IEnumerable<T> TakeWhile<T>(this IEnumerable<T> source, Func<T, bool> predicate, bool inclusive = false)
+    {
+        ArgumentNullException.ThrowIfNull(source, nameof(source));
+        ArgumentNullException.ThrowIfNull(predicate, nameof(predicate));
+
+        foreach (var item in source)
+        {
+            if (predicate(item))
+            {
+                yield return item;
+            }
+            else if (inclusive)
+            {
+                yield return item;
+                yield break;
+            }
+        }
+    }
+}
---
+++ csharp/Advent.Test/Sources/TreetopTreeHouseTestCaseSource.cs
@@ -0,0 +1,11 @@
+﻿using Advent.Solutions;
+
+namespace Advent.Test.Sources;
+
+internal class TreetopTreeHouseTestCaseSource : AdventTestBase<TreetopTreeHouse>
+{
+    public static new IEnumerable<IAdventTestCaseSource> ChallengeTestCaseSources()
+    {
+        yield return new AdventTestCaseSource("30373\n25512\n65332\n33549\n35390", new[] { "21", "8" });
+    }
+}
