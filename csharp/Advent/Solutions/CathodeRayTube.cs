4b6a3e4a19be96fe91a1eb566488e3f94c5504ae
Leif <24778021+LeifWritesCode@users.noreply.github.com>
2022-12-10 14:15:03 +0100

    Implement day 10 part 1

--- csharp/Advent/Solutions/CathodeRayTube.cs
+++ csharp/Advent/Solutions/CathodeRayTube.cs
@@ -1,4 +1,5 @@
 ﻿using Advent.Contracts;
+using Advent.Ornaments;
 using Microsoft.Extensions.Logging;
 
 namespace Advent.Solutions;
@@ -10,10 +11,54 @@ internal class CathodeRayTube : SolutionBase
     {
     }
 
+    private IEnumerable<int> AddX(int x, int v)
+    {
+        yield return x;
+        yield return x + v;
+    }
+
+    private IEnumerable<int> Noop(int x, int v)
+    {
+        yield return x;
+    }
+
     protected override async Task<string> SolvePartOneAsync(IContext context)
     {
+        var pc = 0;
+        var reg = 1;
+        var acc = 0;
+        var v = 0;
+        var line = string.Empty;
+        Func<int, int, IEnumerable<int>> func;
+        var instructions = context.Input.PreProcessLines();
+        var enumerator = instructions.GetEnumerator();
+
+        while (enumerator.MoveNext())
+        {
+            line = (string)enumerator.Current;
+            if (line.StartsWith("addx "))
+            {
+                func = AddX;
+                v = line["addx ".Length..].ToInt();
+            }
+            else
+            {
+                func = Noop;
+            }
+
+            foreach(var result in func(reg, v))
+            {
+                pc++;
+                if (pc == 20 || (pc - 20) % 40 == 0)
+                {
+                    acc += pc * reg;
+                }
+                reg = result;
+            }
+        }
+
         await Task.CompletedTask;
-        return "";
+        return acc.ToString();
     }
 
     protected override async Task<string> SolvePartTwoAsync(IContext context)
