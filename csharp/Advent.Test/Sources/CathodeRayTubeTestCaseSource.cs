25d1de79635418899aceb394a364fb0fef56d985
Leif <24778021+LeifWritesCode@users.noreply.github.com>
2022-12-10 11:28:28 +0100

    Scaffold day 10

---
+++ csharp/Advent.Test/Sources/CathodeRayTubeTestCaseSource.cs
@@ -0,0 +1,11 @@
+﻿using Advent.Solutions;
+
+namespace Advent.Test.Sources;
+
+internal class CathodeRayTubeTestCaseSource : AdventTestBase<CathodeRayTube>
+{
+    public static new IEnumerable<IAdventTestCaseSource> ChallengeTestCaseSources()
+    {
+        yield return new AdventTestCaseSource("", new[] { "", "" });
+    }
+}
---
+++ csharp/Advent/Solutions/CathodeRayTube.cs
@@ -0,0 +1,24 @@
+﻿using Advent.Contracts;
+using Microsoft.Extensions.Logging;
+
+namespace Advent.Solutions;
+
+internal class CathodeRayTube : SolutionBase
+{
+    public CathodeRayTube(ILogger<CathodeRayTube> logger, IInstrument instrument)
+        : base(logger, instrument, 10)
+    {
+    }
+
+    protected override async Task<string> SolvePartOneAsync(IContext context)
+    {
+        await Task.CompletedTask;
+        return "";
+    }
+
+    protected override async Task<string> SolvePartTwoAsync(IContext context)
+    {
+        await Task.CompletedTask;
+        return "";
+    }
+}
