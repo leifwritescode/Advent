4beb133b1013a6ef0cf16906deaaba08397f4301
Leif <24778021+LeifWritesCode@users.noreply.github.com>
2022-12-11 14:25:14 +0100

    Aoc22/11 (#4)

---
+++ csharp/Advent.Test/Sources/MonkeyInTheMiddleTestCaseSource.cs
@@ -0,0 +1,11 @@
+﻿using Advent.Solutions;
+
+namespace Advent.Test.Sources;
+
+internal class MonkeyInTheMiddleTestCaseSource : AdventTestBase<MonkeyInTheMiddle>
+{
+    private static new IEnumerable<AdventTestCaseSource> ChallengeTestCaseSources()
+    {
+        yield return new AdventTestCaseSource("Monkey 0:\r\n  Starting items: 79, 98\r\n  Operation: new = old * 19\r\n  Test: divisible by 23\r\n    If true: throw to monkey 2\r\n    If false: throw to monkey 3\r\n\r\nMonkey 1:\r\n  Starting items: 54, 65, 75, 74\r\n  Operation: new = old + 6\r\n  Test: divisible by 19\r\n    If true: throw to monkey 2\r\n    If false: throw to monkey 0\r\n\r\nMonkey 2:\r\n  Starting items: 79, 60, 97\r\n  Operation: new = old * old\r\n  Test: divisible by 13\r\n    If true: throw to monkey 1\r\n    If false: throw to monkey 3\r\n\r\nMonkey 3:\r\n  Starting items: 74\r\n  Operation: new = old + 3\r\n  Test: divisible by 17\r\n    If true: throw to monkey 0\r\n    If false: throw to monkey 1", new[] { "10605", "2713310158" });
+    }
+}
