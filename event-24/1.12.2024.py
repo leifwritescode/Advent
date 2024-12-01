f48b8dd2d2e66c1212e187524f61a58d6c7e22ab
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2024-12-01 11:32:10 +0100

    Quick note for future leif

--- event-24/1.12.2024.py
+++ event-24/1.12.2024.py
@@ -4,6 +4,7 @@ from collections import Counter
 
 def part_one():
     lines = read_input()
+    # ["1   2", "3   4"] -> [(1, 2), (3, 4)] -> [[1, 3], [2, 4]] -> [1, 3], [2, 4]
     left_list, right_list = zip(*[tuple(map(int, line.split())) for line in lines])
     pairs = zip(sorted(left_list), sorted(right_list))
     return reduce(lambda a, b: a + abs(b[0] - b[1]), pairs, 0)
