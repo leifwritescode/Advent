ddd996a988f8800c7e0231c1cf76ef6caf8d2d3c
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2024-12-01 18:23:04 +0100

    add template

---
+++ event-24/template.py
@@ -0,0 +1,20 @@
+from utilities import read_input
+
+
+def part_one():
+    lines = read_input()
+    return 0
+
+
+def part_two():
+    lines = read_input()
+    return 0
+
+
+def main():
+    print(part_one())
+    print(part_two())
+
+
+if __name__ == '__main__':
+    main()
