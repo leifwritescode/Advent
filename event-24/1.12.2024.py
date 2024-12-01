80de8697a2f271475f0e1ec1ab05b62c7ff65bd5
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2024-12-01 11:05:06 +0100

    Day 1 solution for 2024

---
+++ event-24/1.12.2024.py
@@ -0,0 +1,24 @@
+from utilities import read_input
+from functools import reduce
+from collections import Counter
+
+def part_one():
+    lines = read_input()
+    left_list, right_list = zip(*[tuple(map(int, line.split())) for line in lines])
+    pairs = zip(sorted(left_list), sorted(right_list))
+    return reduce(lambda a, b: a + abs(b[0] - b[1]), pairs, 0)
+
+
+def part_two():
+    lines = read_input()
+    left_list, right_list = zip(*[tuple(map(int, line.split())) for line in lines])
+    right_counts = Counter(right_list)
+    return sum(num * right_counts[num] for num in left_list)
+
+def main():
+    print(part_one())
+    print(part_two())
+
+
+if __name__ == '__main__':
+    main()
