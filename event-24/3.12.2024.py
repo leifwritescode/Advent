be14e8287d2ad90c2d9efd0a3481687315648990
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2024-12-03 09:46:42 +0100

    Day 3 solution

---
+++ event-24/3.12.2024.py
@@ -0,0 +1,34 @@
+from utilities import read_input
+import re
+
+
+def part_one():
+    lines = read_input()
+    matches = [re.finditer("mul\((?P<lhs>\d+),(?P<rhs>\d+)\)", line) for line in lines]
+    return sum([int(m.group("lhs")) * int(m.group("rhs")) for match in matches for m in match])
+
+
+def part_two():
+    lines = read_input()
+    matches = [re.finditer("(?P<do>do\(\))|(?P<dont>don't\(\))|mul\((?P<lhs>\d+),(?P<rhs>\d+)\)", line) for line in lines]
+    count = 0
+    do = True
+    for match in matches:
+        for mo in match:
+            if mo.group("do"):
+                do = True
+            elif mo.group("dont"):
+                do = False
+            else:
+                if do:
+                    count += int(mo.group("lhs")) * int(mo.group("rhs"))
+    return count
+
+
+def main():
+    print(part_one())
+    print(part_two())
+
+
+if __name__ == '__main__':
+    main()
