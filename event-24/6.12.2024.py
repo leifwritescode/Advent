16985857b71a61390fb6478ad853fa5dffd02d4f
Leif Walker-Grant <24778021+leifwritescode@users.noreply.github.com>
2024-12-06 12:46:00 +0100

    Can simplify loop in day 6 part 2

--- event-24/6.12.2024.py
+++ event-24/6.12.2024.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 from utilities import read_input, timed
-from collections import defaultdict
-
+from copy import copy
 
 # guard starts facing up and rotates right 90 degrees when encountering an obstacle
 DIRECTIONS = [ (0, -1), (1, 0), (0, 1), (-1, 0) ]
@@ -52,50 +51,43 @@ def part_one():
     return visited, len(visited)
 
 
-def simulate_guard(map, pos):
+def simulate_guard(map, pos, test):
+    # set the obstruction
+    my_map = copy(map)
+    my_map[test] = True
+    my_pos = copy(pos)
+
     direction = 0 # direction is an index into the direction array. when it changes, we modulo 4.
     visited = set() # true if the cell has been visited
-    while pos in map:
-        record = (pos, direction)
+    while my_pos in my_map:
+        record = (my_pos, direction)
 
         # in part two, if we've visited this cell whilst facing the same direction, we have a loop
         if record in visited:
-            return True # loop
+            return 1 # loop
 
         # record that we've visited current cell
         visited.add(record)
 
         # determine next cell
         vector = DIRECTIONS[direction]
-        next_pos = (pos[0] + vector[0], pos[1] + vector[1])
+        next_pos = (my_pos[0] + vector[0], my_pos[1] + vector[1])
 
         # remembering that map is a collection of booleans
-        if next_pos in map and map[next_pos]:
+        if next_pos in my_map and my_map[next_pos]:
             # rotate
             direction = (direction + 1) % 4
         else:
             # move
-            pos = next_pos
+            my_pos = next_pos
 
-    return False
+    return 0
 
 
 def part_two(path):
     lines = read_input()
     map, pos = extract_map_and_start(lines)
-
-    count = 0
-
-    # for part two, we only need to consider positions on the path visited in part one
-    for cell in path:
-        if map[cell]:
-            continue # ignore if already an obstruction
-
-        map[cell] = True
-        count += 1 if simulate_guard(map, pos) else 0
-        map[cell] = False
-
-    return count
+    return sum(simulate_guard(map, pos, cell) for cell in path if not map[cell])
 
 
 def main():
