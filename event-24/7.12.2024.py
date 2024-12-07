5277edd2e631c4edea2ad08de4d44a9b1beac27e
Leif Walker-Grant <24778021+leifwritescode@users.noreply.github.com>
2024-12-07 21:36:36 +0100

    A suboptimal day 7 part two

--- event-24/7.12.2024.py
+++ event-24/7.12.2024.py
@@ -5,7 +5,7 @@ from utilities import read_input, timed
 # answer = expr + x  // is valid if (answer - x = expr) is valid
 # answer = expr || x // is valid if str(answer) ends with str(x) and (answer.removeSuffix(x) = expr) is valid
 
-def validate_equation(target, operands):
+def validate_equation_part_1(target, operands):
     # if target is not a whole number, then this operation wasn't valid
     if not target.is_integer():
         return False
@@ -22,19 +22,40 @@ def validate_equation(target, operands):
     
     # turns out // is floor division, and / is normal divison
     # otherwise, recurse on the next
-    return validate_equation(target / val, operands[:-1]) or validate_equation(float(target - val), operands[:-1])
+    return validate_equation_part_1(target / val, operands[:-1]) or validate_equation_part_1(target - val, operands[:-1])
 
 # answer is too high
 def part_one():
     lines = read_input()
     lines = [line.split(": ") for line in lines]
     equations = [(float(line[0]), list(map(float, line[1].split()))) for line in lines]
-    return int(sum([equation[0] if validate_equation(equation[0], equation[1]) else 0 for equation in equations]))
+    return int(sum([equation[0] if validate_equation_part_1(equation[0], equation[1]) else 0 for equation in equations]))
+
+
+# for the concatenation pass, we do it forward because the concatenation happens to the current value
+# reversing the concatenation would be challenging
+def validate_equation_part_2(target, current, operands):
+    # if target is not a whole number, then this operation wasn't valid
+    if not current.is_integer():
+        return False
+
+    # if there are no more values, then test to see if current is the target
+    if len(operands) == 0:
+        return target == current
+    
+    # otherwise, recurse through the tree
+    val = operands[0]
+    a = validate_equation_part_2(target, current * val, operands[1:])
+    b = validate_equation_part_2(target, current + val, operands[1:])
+    c = validate_equation_part_2(target, int(f"{int(current)}{int(val)}"), operands[1:])
+    return a or b or c
 
 
 def part_two():
     lines = read_input()
-    return 0
+    lines = [line.split(": ") for line in lines]
+    equations = [(float(line[0]), list(map(float, line[1].split()))) for line in lines]
+    return int(sum([equation[0] if validate_equation_part_2(equation[0], equation[1][0], equation[1][1:]) else 0 for equation in equations]))
 
 
 def main():
