9e5b6702f0abfccbf4a29c109dcc7b099f29e9a4
Leif Walker-Grant <24778021+leifwritescode@users.noreply.github.com>
2024-12-07 21:59:14 +0100

    A far more optimal day 7 part 2
    Orig. runs in 4.1s, new in 0.015s

--- event-24/7.12.2024.py
+++ event-24/7.12.2024.py
@@ -32,30 +32,34 @@ def part_one():
     return int(sum([equation[0] if validate_equation_part_1(equation[0], equation[1]) else 0 for equation in equations]))
 
 
-# for the concatenation pass, we do it forward because the concatenation happens to the current value
-# reversing the concatenation would be challenging
-def validate_equation_part_2(target, current, operands):
+def validate_equation_part_2(target, operands):
     # if target is not a whole number, then this operation wasn't valid
-    if not current.is_integer():
+    if not target.is_integer():
         return False
 
-    # if there are no more values, then test to see if current is the target
-    if len(operands) == 0:
-        return target == current
+    # if we've overshot the target, then the math doesn't check out
+    if target <= 0:
+        return False
+
+    val = operands[-1]
+
+    # if it's the last value, then our target should match it
+    if len(operands) == 1:
+        return val == target
     
-    # otherwise, recurse through the tree
-    val = operands[0]
-    a = validate_equation_part_2(target, current * val, operands[1:])
-    b = validate_equation_part_2(target, current + val, operands[1:])
-    c = validate_equation_part_2(target, int(f"{int(current)}{int(val)}"), operands[1:])
-    return a or b or c
+    a = validate_equation_part_2(target / val, operands[:-1])
+    b = validate_equation_part_2(target - val, operands[:-1])
+    if str(int(target)).endswith(str(int(val))):
+        c = validate_equation_part_2(target // (10 ** len(str(int(val)))), operands[:-1])
+        return a or b or c
+    return a or b
 
 
 def part_two():
     lines = read_input()
     lines = [line.split(": ") for line in lines]
     equations = [(float(line[0]), list(map(float, line[1].split()))) for line in lines]
-    return int(sum([equation[0] if validate_equation_part_2(equation[0], equation[1][0], equation[1][1:]) else 0 for equation in equations]))
+    return int(sum([equation[0] if validate_equation_part_2(equation[0], equation[1]) else 0 for equation in equations]))
 
 
 def main():
