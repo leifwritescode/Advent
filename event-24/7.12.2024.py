7daad7c001f5d5d81a940cb7e0bec99611307305
Leif Walker-Grant <24778021+leifwritescode@users.noreply.github.com>
2024-12-07 21:13:08 +0100

    Day 7 part one

---
+++ event-24/7.12.2024.py
@@ -0,0 +1,49 @@
+from utilities import read_input, timed
+
+# answer = x         // is valid if answer == x
+# answer = expr * x  // is valid if x divides answer and (answer / x = expr) is valid
+# answer = expr + x  // is valid if (answer - x = expr) is valid
+# answer = expr || x // is valid if str(answer) ends with str(x) and (answer.removeSuffix(x) = expr) is valid
+
+def validate_equation(target, operands):
+    # if target is not a whole number, then this operation wasn't valid
+    if not target.is_integer():
+        return False
+
+    # if we've overshot the target, then the math doesn't check out
+    if target <= 0:
+        return False
+
+    val = operands[-1]
+
+    # if it's the last value, then our target should match it
+    if len(operands) == 1:
+        return val == target
+    
+    # turns out // is floor division, and / is normal divison
+    # otherwise, recurse on the next
+    return validate_equation(target / val, operands[:-1]) or validate_equation(float(target - val), operands[:-1])
+
+# answer is too high
+def part_one():
+    lines = read_input()
+    lines = [line.split(": ") for line in lines]
+    equations = [(float(line[0]), list(map(float, line[1].split()))) for line in lines]
+    return int(sum([equation[0] if validate_equation(equation[0], equation[1]) else 0 for equation in equations]))
+
+
+def part_two():
+    lines = read_input()
+    return 0
+
+
+def main():
+    res = timed(lambda: part_one())
+    print("part 1:", res)
+
+    res = timed(lambda: part_two())
+    print("part 2:", res)
+
+
+if __name__ == '__main__':
+    main()
