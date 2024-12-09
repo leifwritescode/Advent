500253fa43baa4dcd8c8728d8905bc3e8bff9fde
Leif Walker-Grant <24778021+leifwritescode@users.noreply.github.com>
2024-12-09 20:33:19 +0000

    enable easy switching to example inputs

--- event-24/utilities.py
+++ event-24/utilities.py
@@ -2,9 +2,13 @@ import __main__
 from pathlib import Path
 import time
 
-def read_input():
+def read_input(example = False):
     path = Path(__main__.__file__)
     input_file_name = path.stem + '.txt'
+
+    if example:
+        input_file_name += "." + "example"
+
     input_file_absolute_path = path.with_name(input_file_name)
     with open(input_file_absolute_path, 'r') as file:
         lines = [line.strip() for line in file]
