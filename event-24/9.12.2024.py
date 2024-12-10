7cc65b2b980dddb9047b433c247e980facd5b9fa
Leif Walker-Grant <24778021+leifwritescode@users.noreply.github.com>
2024-12-10 02:35:19 +0100

    Optimise day 9 part 2
    Cache the free spans in a dictionary of indices to run lengths, then update this each time a file is moved. Since every file is moved exactly once, from right to left, we never need to update the cache with the free space created where the file was. As a result, we can cache once and update as we go. This reduces run time of part 2 to approximately 6.5s from 16m.

--- event-24/9.12.2024.py
+++ event-24/9.12.2024.py
@@ -73,50 +73,75 @@ def find_contiguous_span_of(value, array):
 
     return (start, run)
 
-def find_contiguous_span_of_length(value, array, length, offset):
-    start = -1
-    try:
-        start = array.index(value)
-    except:
-        return (-1, -1)
 
-    run = 1
-    while start + run < len(array) and array[start + run] == value:
-        run +=1
+def cache_free_spans(storage):
+    """
+    Given a storage array, caches the results of find_contiguous_span_of across the full storage
+    """
+    cached_spans = { }
 
-    if length <= run:
-        return (start + offset, run)
-    elif start + run == len(array):
-        return (-1, -1)
-    else:
-        return find_contiguous_span_of_length(value, array[start + run:], length, offset + start + run)
+    offset = 0
+    span = find_contiguous_span_of(-1, storage[offset:])
+    while span[0] != -1:
+        cached_spans[offset + span[0]] = span[1]
+
+        offset += span[0] + span[1]
+        span = find_contiguous_span_of(-1, storage[offset:])
+
+    return cached_spans
+
+
+def try_use_free_span(cached_spans: dict, length, position):
+    """
+    Given the cached free spans, a length of space, and a cut off
+    Returns the index of the first large enough span and updates the cache
+    """
+    keys = [key for key in cached_spans if cached_spans[key] >= length and key < position]
+    if not len(keys):
+        return -1
+
+    # get the smallest key (the earliest span) and its contents
+    index = min(keys)
+    size = cached_spans[index]
+
+    # then, delete that key
+    del cached_spans[index]
+
+    # add a *new* key that is updated with the length used
+    # provided there is space remaining
+    if size - length > 0:
+        cached_spans[index + length] = size - length
+
+    return index
 
 
 def swap_spans(storage, source, dest):
     """
     swaps two spans of data in the array
+    source is a (start, run) and dest is an index
     """
     source_val = storage[source[0]]
     # dest_val is always -1
 
-    for x in range(dest[0], dest[0] + source[1]):
+    for x in range(dest, dest + source[1]):
         storage[x] = source_val
 
     for x in range(source[0], source[0] + source[1]):
         storage[x] = -1
 
 
-def compress_storage_by_span(storage):
+def compress_storage_by_span(storage, cached_spans):
     # since we know that there is no free blocks at the end of the array
     # we can cheese finding the first file_id
     file_id = storage[-1]
 
     while file_id > 0: # file_id 0 is guaranteed to be at the start
-        print("processing", file_id)
         file_span = find_contiguous_span_of(file_id, storage)
-        free_span = find_contiguous_span_of_length(-1, storage[:file_span[0]], file_span[1], 0)
-        if free_span[0] != -1:
+        free_span = try_use_free_span(cached_spans, file_span[1], file_span[0])
+
+        if free_span != -1:
             swap_spans(storage, file_span, free_span)
+
         file_id -= 1
 
 
@@ -124,7 +149,8 @@ def part_two(input):
     files, spaces = input
 
     storage = expand_drive_map(files, spaces)
-    compress_storage_by_span(storage)
+    cached_spans = cache_free_spans(storage)
+    compress_storage_by_span(storage, cached_spans)
 
     return sum([x[0] * x[1] for x in enumerate(storage) if x[1] != -1])
 
@@ -136,16 +162,9 @@ def main():
     res = timed(lambda: part_one(input))
     print("part 1:", res if res != -1 else "not implemented")
 
-    # 6418529470362 after 16 minutes lmfao
-    # we can try to optimise by recording the contiguous free memory ONCE
-    # and then updating that map each time we change
-    # if we use a map of { index : space }, then we should be able to say 
-    # min(key) for key in dict if dict[key] <= n
-    # where n is the amount of space required
-    # then update the dict using dict[key] = dict[key] - n
-    # and clearing the key if it zeroes out
-    # res = timed(lambda: part_two(input))
-    # print("part 2:", res if res != -1 else "not implemented")
+    # ~6.5s after optimisation
+    res = timed(lambda: part_two(input))
+    print("part 2:", res if res != -1 else "not implemented")
 
 
 if __name__ == '__main__':
