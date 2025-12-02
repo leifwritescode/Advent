a154b49a7fd3759fecae36de3d15e83a9e364ca3
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2025-12-02 14:52:16 +0100

    2025 day 2 part 2 + minor optimisation to part 1

--- event-25/02.12.2025.py
+++ event-25/02.12.2025.py
@@ -1,5 +1,5 @@
 from utilities import read_input, timed
-from math import log10, floor, pow
+from math import log10, floor
 
 def prepare_input():
     lines = read_input()
@@ -30,10 +30,47 @@ def bifurcate_and_test(i):
     k = floor(log10(i))+1
     if (k % 2): return 0
     h_k = k // 2
-    d = pow(10, h_k)
-    i_l = floor(i // d)
+    d = 10 ** h_k
+    i_l = i // d
     i_r = i % d
     return i if i_l == i_r else 0
+
+# lookup table for the set of
+# values n satisfying 2 <= n <= d
+# for some natural integer d satisfying d <= 10
+# where d % n = 0
+m_s = [
+    [ ],
+    [ ],
+    [ 2 ],
+    [ 3 ],
+    [ 2, 4 ],
+    [ 5 ],
+    [ 2, 3, 6 ],
+    [ 7 ],
+    [ 2, 4, 8 ],
+    [ 3, 9 ],
+    [ 2, 5, 10 ]
+]
+
+def birfurcate_and_test_maximal(i):
+    """
+    for a given natural integer i
+    d = floor( log10( i ) ) + 1
+    m = set of all values n satisfying 2 <= n <= d where d % n = 0
+    for each value m_i of m
+    k = d / m_i
+    b = floor( i / 10^k( m_i - 1) )
+    if i = b dot (10^km - 1 / 10^k - 1)
+    """
+    d = floor(log10(i))+1
+    m = m_s[d]
+    for m_i in m:
+        k = d // m_i
+        b = i // (10 ** (k * (m_i - 1)))
+        r = b * (10 ** (k * m_i) - 1) // ((10 ** k) - 1)
+        if r == i: return i
+    return 0
 
 
 def part_one(input):
@@ -41,7 +78,7 @@ def part_one(input):
 
 
 def part_two(input):
-    return -1
+    return sum([birfurcate_and_test_maximal(i) for i in input])
 
 
 def main():
